8b9a77de8296e0179b59d8de13a6893ed2433d2f
wenlinaghuang <wen-liang.huang@cern.ch>
2016-05-15 20:36:01 +0800

    2016.5.15.2035

--- src/parallel_computing.cc
+++ src/parallel_computing.cc
@@ -4,22 +4,25 @@
 #include <cstdlib>
 #include "../lib/tm_usage.h"
 #include <iostream>
+#define N 100
 using namespace std;
-void Test1(int n)
+/*void Test1(int n)
 {
-	for(int i =0;i<100000;++i)
+	for(int i=0;i<100000;++i)
 	{
+	cout << i << " ";
 		
 	}
-	
+cout << endl;
 cout << n <<endl;
 }
 void Test2(int n)
 {
 	for(int i=0;i<100000;++i)
 	{
-	
+	cout << i << " " ;	
 	}
+	cout << endl;
 cout << n << endl;
 }
 int main()
@@ -28,17 +31,49 @@ int main()
 	CommonNs::TmStat  stat;
 	
 	tmusg.periodStart();
-	for(int i =0;i<10;++i)
+	for(int i =0;i<5;++i)
 		Test1(i);
 	tmusg.getPeriodUsage(stat);
-	cout << "# run time 1 = " << (stat.uTime+stat.sTime)/1000000.0 << "sec" <<endl;
 	
 	CommonNs::TmUsage tmusg2;
 	CommonNs::TmStat stat2;
 	tmusg2.periodStart();
 	#pragma omp parallel for
-	for(int i = 0;i<10;++i)
+	for(int i = 0;i<5;++i)
 		Test2(i);
 	tmusg2.getPeriodUsage(stat2);
-	cout << "# run time 1 = " << (stat2.uTime+stat2.sTime)/1000000.0 << "sec"     <<endl;
-}
+	cout << "# run time 1 = " << (stat.uTime+stat.sTime)/1000000.0 << "sec" <<endl;
+	cout << "# run time 2 = " << (stat2.uTime+stat2.sTime)/1000000.0 << "sec"     <<endl;
+}*/
+
+
+
+
+
+
+int main()
+ {   
+     int nthreads,tid,i;
+     float a[N],b[N],c[N];
+     
+     for(int i = 0;i<N;i++)
+         a[i] = b[i] = i;
+     #pragma omp parallel shared(a,b,c,threads) private(i,itd)
+     {
+         tid = omp_get_thread_num();
+         if(tid == 0)
+         {
+             nthreads = omp_get_num_threads();
+             cout << "Number of threads = " << nthreads<<endl;
+         }
+         
+         cout << "Thread " << tid << "starting..."<<endl;
+     #pragma omp for
+     for(int i = 0;i<N;i++)
+     {                                                                                                                                                               
+         c[i] = a[i] + b[i];
+         cout << "Thread " << tid << ": c["<<i<<"] = " << c[i] <<endl;
+     }
+     }
+ } 
+
